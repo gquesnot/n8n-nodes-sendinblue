59dd833c37ecad78de3c98c5090c047bc1f3de45
gquesnot <gaqu1994@gmail.com>
2021-07-20 11:03:15 +0200

    done

--- update.sh
+++ update.sh
@@ -5,17 +5,26 @@ git pull
 yes | cp -rf dist/nodes/*  /home/forge/n8n.marquedigitale.fr/packages/nodes-base/nodes
 yes | cp -rf dist/credentials/*  /home/forge/n8n.marquedigitale.fr/packages/nodes-base/credentials/
 cd /home/forge/n8n.marquedigitale.fr/packages/nodes-base/ || return
-if grep -q "Sendinblue" package.json; test $? -eq 1; then
-  match='''"credentials":\ ['''
-  insert='''"dist/credentials/SendinblueApi.credentials.js"'''
-  sed -i "s/'"$match"'/'"$match"'\n'"$insert"'/" package.json
-  match='''"nodes":\ ['''
-  insert='''      "dist/nodes/Sendinblue/Sendinblue.node.js"'''
-  sed -i "s/'"$match"'/'"$match"'\n'"$insert"'/" package.json
-    match='''  \"dependencies\":\ {'''
-  insert='''    "sib-api-v3-sdk": "^8.2.1"'''
-  sed -i "s/'"$match"'/'"$match"'\n'"$insert"'/" package.json
+if grep -q "Sendinblue.credentials.js" package.json; test $? -eq 1; then
+  echo 1
+  match='    "credentials": ['
+  insert='      "dist/credentials/SendinblueApi.credentials.js"'
+  sed -i "s/$match/$match\n$insert/" package.json
 fi
+if grep -q "Sendinblue.node.js" package.json; test $? -eq 1; then
+  echo 2
+  match='    "nodes": ['
+  insert='      "dist/nodes/Sendinblue/Sendinblue.node.js"'
+  sed -i "s/$match/$match\n$insert/" package.json
+fi
+if grep -q "sib-api-v3-sdk" package.json; test $? -eq 1; then
+  echo 3
+  match='  \"dependencies\": {'
+  insert='    "sib-api-v3-sdk": "^8.2.1"'
+  sed -i "s/$match/$match\n$insert/" package.json
+fi
+
+
 cd /home/forge/n8n.marquedigitale.fr || return
 lerna bootstrap --hoist
 npm run build

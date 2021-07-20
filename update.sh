112566b18eef388a6de2d57c33284551b61617e9
gquesnot <gaqu1994@gmail.com>
2021-07-20 10:56:05 +0200

    done

--- update.sh
+++ update.sh
@@ -6,13 +6,13 @@ yes | cp -rf dist/nodes/*  /home/forge/n8n.marquedigitale.fr/packages/nodes-base
 yes | cp -rf dist/credentials/*  /home/forge/n8n.marquedigitale.fr/packages/nodes-base/credentials/
 cd /home/forge/n8n.marquedigitale.fr/packages/nodes-base/ || return
 if grep -q "Sendinblue" package.json; test $? -eq 1; then
-  match='''    "credentials": ['''
-  insert='''      "dist/credentials/SendinblueApi.credentials.js"'''
+  match='''"credentials":\ ['''
+  insert='''"dist/credentials/SendinblueApi.credentials.js"'''
   sed -i "s/$match/$match\n$insert/" package.json
-  match='''    "nodes": ['''
+  match='''"nodes":\ ['''
   insert='''      "dist/nodes/Sendinblue/Sendinblue.node.js"'''
   sed -i "s/$match/$match\n$insert/" package.json
-    match='''  \"dependencies\": {'''
+    match='''  \"dependencies\":\ {'''
   insert='''    "sib-api-v3-sdk": "^8.2.1"'''
   sed -i "s/$match/$match\n$insert/" package.json
 fi

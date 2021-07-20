7dd2ae68e0f7c5729f90c67407c3f724df5431f7
gquesnot <gaqu1994@gmail.com>
2021-07-20 10:28:33 +0200

    done

--- update.sh
+++ update.sh
@@ -6,9 +6,9 @@ yes | cp -rf dist/nodes/*  /home/forge/n8n.marquedigitale.fr/packages/nodes-base
 yes | cp -rf dist/credentials/*  /home/forge/n8n.marquedigitale.fr/packages/nodes-base/credentials/
 cd /home/forge/n8n.marquedigitale.fr/packages/nodes-base/ || return
 if grep -q "Sendinblue" package.json; test $? -eq 1; then
-  sed 's/"credentials": [/"credentials": [\n"dist/credentials/SendinblueApi.credentials.js",/' package.json
-  sed 's/"nodes": [\n"dist/nodes/Sendinblue/Sendinblue.node.js",/'package.json
-  sed 's/"dependencies": {\n"sib-api-v3-sdk": "^8.2.1"/' package.json
+  sed 's/"credentials": [/"credentials": [\n"      dist/credentials/SendinblueApi.credentials.js",/' package.json
+  sed 's/"nodes": [\n"      dist/nodes/Sendinblue/Sendinblue.node.js",/'package.json
+  sed 's/"dependencies": {\n    "sib-api-v3-sdk": "^8.2.1"/' package.json
   npm i
 fi
 cd /home/forge/n8n.marquedigitale.fr || return

274c2244f2656cd6aff84059fb68ca48559124af
gquesnot <gaqu1994@gmail.com>
2021-07-20 09:57:24 +0200

    done

--- update.sh
+++ update.sh
@@ -4,22 +4,14 @@ cd ../tmp/n8n-nodes-sendinblue || return
 git pull
 cp -rf dist/nodes/  /home/forge/n8n.marquedigitale.fr/package/nodes-base/nodes
 cp -rf dist/credentials/  /home/forge/n8n.marquedigitale.fr/package/nodes-base/nodes/credentials/
-sed 's/"credentials": [/"credentials": [\n"dist/credentials/SendinblueApi.credentials.js",/' n8n.marquedigitale.fr/package/nodes-base/package.json
-sed 's/"nodes": [\n"dist/nodes/Sendinblue/Sendinblue.node.js",/' n8n.marquedigitale.fr/package/nodes-base/package.json
-sed 's/"dependencies": {\n"sib-api-v3-sdk": "^8.2.1"/' n8n.marquedigitale.fr/package/nodes-base/package.json
 cd n8n.marquedigitale.fr/package/nodes-base/ || return
-npm i
-cd ../node-dev || return
-npm i
-cd ../workflow || return
-npm i
-cd ../editor-ui || return
-npm i
-cd ../core || return
-npm i
-cd ../cli || return
-npm i
-cd ../../
+if grep -q "Sendinblue" package.json; test $? -eq 1; then
+  sed 's/"credentials": [/"credentials": [\n"dist/credentials/SendinblueApi.credentials.js",/' n8n.marquedigitale.fr/package/nodes-base/package.json
+  sed 's/"nodes": [\n"dist/nodes/Sendinblue/Sendinblue.node.js",/' n8n.marquedigitale.fr/package/nodes-base/package.json
+  sed 's/"dependencies": {\n"sib-api-v3-sdk": "^8.2.1"/' n8n.marquedigitale.fr/package/nodes-base/package.json
+  npm i
+fi
+cd /home/forge/n8n.marquedigitale.fr || return
 lerna bootstrap --hoist
 npm run build
 (npm run start&)

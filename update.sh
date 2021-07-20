a6d2782a95a7533152be7c82b9a24beabac3b890
gquesnot <gaqu1994@gmail.com>
2021-07-20 09:59:08 +0200

    done

--- update.sh
+++ update.sh
@@ -2,8 +2,8 @@ cd /home/forge/n8n.marquedigitale.fr || return
 git pull
 cd ../tmp/n8n-nodes-sendinblue || return
 git pull
-cp -rf dist/nodes/  /home/forge/n8n.marquedigitale.fr/package/nodes-base/nodes
-cp -rf dist/credentials/  /home/forge/n8n.marquedigitale.fr/package/nodes-base/nodes/credentials/
+cp -rf dist/nodes/  /home/forge/n8n.marquedigitale.fr/packages/nodes-base/nodes
+cp -rf dist/credentials/  /home/forge/n8n.marquedigitale.fr/packages/nodes-base/nodes/credentials/
 cd n8n.marquedigitale.fr/package/nodes-base/ || return
 if grep -q "Sendinblue" package.json; test $? -eq 1; then
   sed 's/"credentials": [/"credentials": [\n"dist/credentials/SendinblueApi.credentials.js",/' n8n.marquedigitale.fr/package/nodes-base/package.json

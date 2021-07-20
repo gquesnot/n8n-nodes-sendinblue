d632f413d09cfa2dd5d8794273d591e0e95f9094
gquesnot <gaqu1994@gmail.com>
2021-07-20 10:01:04 +0200

    done

--- update.sh
+++ update.sh
@@ -4,11 +4,11 @@ cd ../tmp/n8n-nodes-sendinblue || return
 git pull
 cp -rf dist/nodes/  /home/forge/n8n.marquedigitale.fr/packages/nodes-base/nodes
 cp -rf dist/credentials/  /home/forge/n8n.marquedigitale.fr/packages/nodes-base/credentials/
-cd /home/forge/n8n.marquedigitale.fr/package/nodes-base/ || return
+cd /home/forge/n8n.marquedigitale.fr/packages/nodes-base/ || return
 if grep -q "Sendinblue" package.json; test $? -eq 1; then
-  sed 's/"credentials": [/"credentials": [\n"dist/credentials/SendinblueApi.credentials.js",/' n8n.marquedigitale.fr/package/nodes-base/package.json
-  sed 's/"nodes": [\n"dist/nodes/Sendinblue/Sendinblue.node.js",/' n8n.marquedigitale.fr/package/nodes-base/package.json
-  sed 's/"dependencies": {\n"sib-api-v3-sdk": "^8.2.1"/' n8n.marquedigitale.fr/package/nodes-base/package.json
+  sed 's/"credentials": [/"credentials": [\n"dist/credentials/SendinblueApi.credentials.js",/' package.json
+  sed 's/"nodes": [\n"dist/nodes/Sendinblue/Sendinblue.node.js",/'package.json
+  sed 's/"dependencies": {\n"sib-api-v3-sdk": "^8.2.1"/' package.json
   npm i
 fi
 cd /home/forge/n8n.marquedigitale.fr || return

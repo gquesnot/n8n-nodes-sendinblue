fdcbd74ff09c0868b5c47e9579efa12a6948b71d
gquesnot <gaqu1994@gmail.com>
2021-07-20 12:21:16 +0200

    done

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ git pull
 yes | cp -rf dist/nodes/*  /home/forge/n8n.marquedigitale.fr/packages/nodes-base/nodes
 yes | cp -rf dist/credentials/*  /home/forge/n8n.marquedigitale.fr/packages/nodes-base/credentials/
 cd /home/forge/n8n.marquedigitale.fr/packages/nodes-base/ || return
-if grep -q "Sendinblue.credentials.js" package.json; test $? -eq 1; then
+if grep -q "SendinblueApi.credentials.js" package.json; test $? -eq 1; then
   echo 1
   match='    \"credentials\": \['
   insert='      \"dist/credentials/SendinblueApi.credentials.js\",'
@@ -13,16 +13,15 @@ if grep -q "Sendinblue.credentials.js" package.json; test $? -eq 1; then
 fi
 if grep -q "Sendinblue.node.js" package.json; test $? -eq 1; then
   echo 2
-  match='    \"nodes\": ['
+  match='    \"nodes\": \['
   insert='      \"dist/nodes/Sendinblue/Sendinblue.node.js\",'
   sed -i "s|$match|$match\n$insert|" package.json
-
 fi
 if grep -q "sib-api-v3-sdk" package.json; test $? -eq 1; then
   echo 3
   match='  \"dependencies\": {'
   insert='    "sib-api-v3-sdk": "\^8.2.1",'
-  sed -i "s/$match//$match\n$insert/" package.json
+  sed -i "s/$match/$match\n$insert/" package.json
   npm i
 fi
 
